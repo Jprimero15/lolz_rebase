15a306edfa2b3c9650804a760f3b93da4ff97c20
Sai Charan Gurrappadi <sgurrappadi@nvidia.com>
2012-08-24 18:42:36 -0700

    cpuquiet: Update averaging of nr_runnables
    Doing a Exponential moving average per nr_running++/-- does not
    guarantee a fixed sample rate which induces errors if there are lots of
    threads being enqueued/dequeued from the rq (Linpack mt). Instead of
    keeping track of the avg, the scheduler now keeps track of the integral
    of nr_running and allows the readers to perform filtering on top.
    
    Implemented a proper exponential moving average for the runnables
    governor and a straight 100ms average for the balanced governor. Tweaked
    the thresholds for the runnables governor to minimize latency. Also,
    decreased sample_rate for the runnables governor to the absolute minimum
    of 10msecs.
    
    Updated to K3.4
    
    Change-Id: Ia25bf8baf2a1a015ba188b2c06e551e89b16c5f8
    Signed-off-by: Sai Charan Gurrappadi <sgurrappadi@nvidia.com>
    Signed-off-by: Peter De Schrijver <pdeschrijver@nvidia.com>
    Reviewed-on: http://git-master/r/131147
    Reviewed-by: Juha Tukkinen <jtukkinen@nvidia.com>
    
    Rebase-Id: R7a20292e2cfb551a875962f0903647f69b78a0ab

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -2331,18 +2331,6 @@ unsigned long avg_nr_running(void)
 }
 EXPORT_SYMBOL(avg_nr_running);
 
-unsigned long get_avg_nr_running(unsigned int cpu)
-{
-        struct rq *q;
-
-        if (cpu >= nr_cpu_ids)
-                return 0;
-
-        q = cpu_rq(cpu);
-
-        return q->ave_nr_running;
-}
-
 unsigned long avg_cpu_nr_running(unsigned int cpu)
 {
 	unsigned int seqcnt, ave_nr_running;
@@ -2375,6 +2363,34 @@ unsigned long this_cpu_loadx(int i)
 	return this->cpu_load[i];
 }
 #endif /* CONFIG_RUNTIME_COMPCACHE */
+
+u64 nr_running_integral(unsigned int cpu)
+{
+        unsigned int seqcnt;
+        u64 integral;
+        struct rq *q;
+
+        if (cpu >= nr_cpu_ids)
+                return 0;
+
+        q = cpu_rq(cpu);
+
+        /*
+         * Update average to avoid reading stalled value if there were
+         * no run-queue changes for a long time. On the other hand if
+         * the changes are happening right now, just read current value
+         * directly.
+         */
+
+        seqcnt = read_seqcount_begin(&q->ave_seqcnt);
+        integral = do_nr_running_integral(q);
+        if (read_seqcount_retry(&q->ave_seqcnt, seqcnt)) {
+                read_seqcount_begin(&q->ave_seqcnt);
+                integral = q->nr_running_integral;
+        }
+
+        return integral;
+}
 
 /*
  * Global load-average calculations
