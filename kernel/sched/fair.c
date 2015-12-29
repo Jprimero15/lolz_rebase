3d45589cb4bf9a6a059918252b6099ca4f70b397
franciscofranco <franciscofranco.1990@gmail.com>
2015-12-29 04:22:20 +0000

    sched/fair: rq might've been uninitialized, so fix it
    Signed-off-by: franciscofranco <franciscofranco.1990@gmail.com>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -5322,6 +5322,8 @@ static void nohz_idle_balance(int this_cpu, enum cpu_idle_type idle)
 		if (need_resched())
 			break;
 
+		rq = cpu_rq(balance_cpu);
+
 		/*
 		 * If time for next balance is due,
 		 * do the balance.
@@ -5334,7 +5336,6 @@ static void nohz_idle_balance(int this_cpu, enum cpu_idle_type idle)
 			rebalance_domains(balance_cpu, CPU_IDLE);
 		}
 
-		rq = cpu_rq(balance_cpu);
 		if (time_after(this_rq->next_balance, rq->next_balance))
 			this_rq->next_balance = rq->next_balance;
 	}
