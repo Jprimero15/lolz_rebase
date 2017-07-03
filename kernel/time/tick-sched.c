97f4b9e8b7d37881da597bed2fa32a6d107cae0a
Gobinda Joy <gobinda.joy@gmail.com>
2017-07-03 16:39:57 +0600

    drivers: Update cpuidle remove power_usage

--- kernel/time/tick-sched.c
+++ kernel/time/tick-sched.c
@@ -513,6 +513,9 @@ void tick_nohz_irq_exit(void)
 
 	local_irq_save(flags);
 
+	/* Cancel the timer because CPU already waken up from the C-states*/
+	menu_hrtimer_cancel();
+
 	tick_nohz_stop_sched_tick(ts);
 
 	local_irq_restore(flags);
@@ -577,6 +580,9 @@ void tick_nohz_idle_exit(void)
 	WARN_ON_ONCE(!ts->inidle);
 
 	ts->inidle = 0;
+
+	/* Cancel the timer because CPU already waken up from the C-states*/
+	menu_hrtimer_cancel();
 
 	if (ts->idle_active || ts->tick_stopped)
 		now = ktime_get();
