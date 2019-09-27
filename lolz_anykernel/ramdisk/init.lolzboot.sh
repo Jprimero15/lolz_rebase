30e1310581a9310167e25e8c9f84f1b39c926179
Joshua Primero <jprimero155@gmail.com>
2019-09-27 14:06:49 +0000

    LOLZ: boot_script: Enable CPUQUIET and tweak
    Signed-off-by: Joshua Primero <jprimero155@gmail.com>

--- lolz_anykernel/ramdisk/init.lolzboot.sh
+++ lolz_anykernel/ramdisk/init.lolzboot.sh
@@ -45,8 +45,10 @@ if [ "$(grep -c Lol /proc/version)" -eq "1" ]; then
     echo "noop" > /sys/block/mmcblk1/queue/scheduler
     echo "noop" > /sys/block/mmcblk0/queue/scheduler
 
-    # Enable Bricked Hotplug
-    echo "1" > /sys/kernel/msm_mpdecision/conf/enabled
+    # Enable RQbalance and Tweaks if boot completed
+    echo "rqbalance" > /sys/devices/system/cpu/cpuquiet/current_governor
+    echo "268000 0" > /sys/devices/system/cpu/cpuquiet/rqbalance/idle_bottom_freq
+    echo "960000 0" > /sys/devices/system/cpu/cpuquiet/rqbalance/idle_top_freq
 
     # We are done here
     echo "LolZ-Kernel: Boot Script Executed Sucesfully !! " | tee /dev/kmsg
