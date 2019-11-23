353645444af4e225e0b87009b6bc512baec1a5db
Joshua Primero <jprimero155@gmail.com>
2019-11-23 13:38:24 +0000

    Revert "More power-efficient workqueues"
    This reverts commit 434b4f1e596e5fdeb4113472a12d5b7a2dbcfcab.

--- arch/arm/mach-msm/bam_dmux.c
+++ arch/arm/mach-msm/bam_dmux.c
@@ -1618,7 +1618,7 @@ static void ul_timeout(struct work_struct *work)
 		return;
 	ret = write_trylock_irqsave(&ul_wakeup_lock, flags);
 	if (!ret) { /* failed to grab lock, reschedule and bail */
-		queue_delayed_work(system_power_efficient_wq, &ul_timeout_work,
+		schedule_delayed_work(&ul_timeout_work,
 				msecs_to_jiffies(UL_TIMEOUT_DELAY));
 		return;
 	}
@@ -1647,7 +1647,7 @@ static void ul_timeout(struct work_struct *work)
 			BAM_DMUX_LOG("%s: pkt written %d\n",
 				__func__, ul_packet_written);
 			ul_packet_written = 0;
-			queue_delayed_work(system_power_efficient_wq, &ul_timeout_work,
+			schedule_delayed_work(&ul_timeout_work,
 					msecs_to_jiffies(UL_TIMEOUT_DELAY));
 		} else {
 			ul_powerdown();
@@ -1733,7 +1733,7 @@ static void ul_wakeup(void)
 		}
 		if (likely(do_vote_dfab))
 			vote_dfab();
-		queue_delayed_work(system_power_efficient_wq, &ul_timeout_work,
+		schedule_delayed_work(&ul_timeout_work,
 				msecs_to_jiffies(UL_TIMEOUT_DELAY));
 		bam_is_connected = 1;
 		mutex_unlock(&wakeup_lock);
@@ -1778,7 +1778,7 @@ static void ul_wakeup(void)
 
 	bam_is_connected = 1;
 	BAM_DMUX_LOG("%s complete\n", __func__);
-	queue_delayed_work(system_power_efficient_wq, &ul_timeout_work,
+	schedule_delayed_work(&ul_timeout_work,
 				msecs_to_jiffies(UL_TIMEOUT_DELAY));
 	mutex_unlock(&wakeup_lock);
 }
