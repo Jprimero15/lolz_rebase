4163123cc959bf586fd847d8c93a03a5428aecbb
Joshua Primero <jprimero155@gmail.com>
2019-11-24 00:32:38 +0000

    LOLZ: AK3: Initial Android 10 Support (TRY HARDER)
    * Check Android Versions on anykernel.sh
    * disabling of MPDecision on anykernel.sh
    * add overlay.d
    * boot_script: add init.lolzboot.sh and init.lolz.rc
    * Online All Cores Early
    * bypass safetynet hack
    
    Signed-off-by: Joshua Primero <jprimero155@gmail.com>

--- lolz_anykernel/anykernel.sh
+++ lolz_anykernel/anykernel.sh
@@ -5,7 +5,7 @@
 ## AnyKernel setup
 # begin properties
 properties() { '
-kernel.string=*    LolZ-Kernel For Android 9
+kernel.string=* LolZ-Kernel For Android 9 and 10
 do.devicecheck=1
 do.modules=0
 do.cleanup=1
@@ -18,12 +18,12 @@ device.name6=hlteskt
 device.name7=hltespr
 device.name8=hltetmo
 device.name9=hltexx
-supported.versions=9.0.0, 9
+supported.versions=9 - 10
 '; } # end properties
 
 # shell variables
 block=/dev/block/platform/msm_sdcc.1/by-name/boot;
-is_slot_device=0;
+is_slot_device=auto;
 ramdisk_compression=auto;
 
 
@@ -34,7 +34,7 @@ ramdisk_compression=auto;
 
 ## AnyKernel file attributes
 # set permissions/ownership for included ramdisk files
-chmod -R 750 $ramdisk/*;
+set_perm_recursive 0 0 750 750 $ramdisk/*;
 chmod -R 640 $ramdisk/fstab.qcom;
 chmod -R 644 $ramdisk/ueventd.qcom.rc;
 chown -R root:root $ramdisk/*;
@@ -45,7 +45,36 @@ dump_boot;
 
 # begin ramdisk changes
 
-# oof
+# Mount System
+mount -o rw,remount -t auto /system;
+
+# Get Android Version from /system/vendor
+OSV="$(file_getprop /system/vendor/build.prop ro.vendor.build.version.release)";
+#  Check Android Version
+if [ "$OSV" == "10" ] || [ "$OSV" == "10.0" ] || [ "$OSV" == "10.0.0" ]; then
+  ui_print "- Android 10 Detected!";
+  ui_print "- Configuring...";
+  rm $ramdisk/fstab.qcom;
+  rm $ramdisk/init.lolz.rc;
+  rm $ramdisk/init.lolzboot.sh;
+  rm $ramdisk/init.qcom.rc;
+  rm $ramdisk/init.target.rc;
+ else
+  ui_print "- Android 9(PIE) Detected!";
+  ui_print "- Configuring...";
+  rm -rf $ramdisk/overlay.d;
+fi;
+  ui_print "- MPDecision Found! Disabling..."
+if [ -f /system/vendor/bin/mpdecision ]; then
+  mv /system/vendor/bin/mpdecision /system/vendor/bin/mpdecision.bak;
+  ui_print "- MPDecision Disabled... Yeah!!";
+ else
+  ui_print "- MPDecision Disabled... Yeah!!";
+fi;
+  ui_print "- Configuration has been Set!!";
+
+# UnMount System
+mount -o ro,remount -t auto /system;
 
 # end ramdisk changes
 
