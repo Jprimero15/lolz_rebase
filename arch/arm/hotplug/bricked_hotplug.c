7ecf0eff01079ff8d740f7fcc69705e9c9e2f9a4
Joshua Primero <jprimero155@gmail.com>
2019-12-07 23:09:14 +0000

    ARM: bricked_hotplug: ReTweak it
    Signed-off-by: Joshua Primero <jprimero155@gmail.com>

--- arch/arm/hotplug/bricked_hotplug.c
+++ arch/arm/hotplug/bricked_hotplug.c
@@ -32,8 +32,8 @@
 
 #define MPDEC_TAG			"bricked_hotplug"
 #define HOTPLUG_ENABLED			1
-#define MSM_MPDEC_DELAY			130
-#define MSM_MPDEC_IDLE_FREQ		1267200
+#define MSM_MPDEC_DELAY			100
+#define MSM_MPDEC_IDLE_FREQ		1728000
 #define MSM_MPDEC_STARTDELAY		10000
 #define DEFAULT_MIN_CPUS_ONLINE		1
 #define DEFAULT_MAX_CPUS_ONLINE		NR_CPUS
@@ -79,7 +79,7 @@ static struct cpu_hotplug {
 	.bricked_enabled = HOTPLUG_ENABLED,
 };
 
-static unsigned int NwNs_Threshold[8] = {20, 0, 26, 16, 36, 18, 0, 20};
+static unsigned int NwNs_Threshold[8] = {12, 0, 20, 7, 25, 10, 0, 18};
 static unsigned int TwTs_Threshold[8] = {140, 0, 140, 190, 140, 190, 0, 190};
 
 struct down_lock {
