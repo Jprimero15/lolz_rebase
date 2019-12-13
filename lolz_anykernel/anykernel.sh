805c8f5a22e004975c26daddfe6012df89c07e84
Joshua Primero <jprimero155@gmail.com>
2019-12-13 16:17:02 +0000

    LOLZ: AK3: Disable Thermal-engine and thermal libs
    * Also disabling Thermal libs since it cause delays on torch and camera if thermal-egine doesn't work
    * Give all of the way for Simple Thermal driver
    
    Signed-off-by: Joshua Primero <jprimero155@gmail.com>

--- lolz_anykernel/anykernel.sh
+++ lolz_anykernel/anykernel.sh
@@ -69,10 +69,23 @@ fi;
 # Check Mpdecision
 if [ -f /system/vendor/bin/mpdecision ]; then
   ui_print "- MPDecision Found! Disabling..."
-  mv /system/vendor/bin/mpdecision /system/vendor/bin/mpdecision.bak;
+  mv /system/vendor/bin/mpdecision /system/vendor/bin/lolz.mpdecision;
   ui_print "- MPDecision has been Disabled!";
  else
   ui_print "- MPDecision is Disabled!!";
+fi;
+
+# Check Thermal Driver
+if [ -f /system/vendor/bin/thermal-engine ] || [ -f /system/vendor/lib/libthermalclient.so ] || [ -f /system/vendor/lib/libthermalioctl.so ]; then
+  ui_print "- Stock Thermal Found! Disabling..."
+  mv /system/vendor/bin/thermal-engine /system/vendor/bin/lolz.thermal-engine;
+  mv /system/vendor/lib/libthermalclient.so /system/vendor/lib/lolz.libthermalclient.so;
+  mv /system/vendor/lib/libthermalioctl.so /system/vendor/lib/lolz.libthermalclient.so;
+  ui_print "- Stock Thermal has been Disabled!";
+  ui_print "- Enabling New Thermal Driver...";
+  ui_print "- New Thermal Driver Enabled!!";
+ else
+  ui_print "- New Thermal Driver is Enabled!!";
 fi;
 
 # UnMount System
