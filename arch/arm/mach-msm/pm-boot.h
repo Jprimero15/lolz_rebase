30623d62282b2325914e8c26444c52810510ec9a
Jprimero15 <jprimero15@aospa.co>
2026-09-04 13:17:20 +0000

    ARM: msm: pm-boot: Fix init section
    mismatch Mark msm_pm_boot_init() as
    __devinit instead of __init since it is
    called from msm_pm_boot_probe(), which can
    execute outside the init
    section. This prevents the section mismatch warning during MODPOST.

--- arch/arm/mach-msm/pm-boot.h
+++ arch/arm/mach-msm/pm-boot.h
@@ -31,9 +31,9 @@ struct msm_pm_boot_platform_data {
 };
 
 #ifdef CONFIG_PM
-int __init msm_pm_boot_init(struct msm_pm_boot_platform_data *pdata);
+int __devinit msm_pm_boot_init(struct msm_pm_boot_platform_data *pdata);
 #else
-static inline int __init msm_pm_boot_init(
+static inline int __devinit msm_pm_boot_init(
 		struct msm_pm_boot_platform_data *pdata)
 {
 	return 0;
